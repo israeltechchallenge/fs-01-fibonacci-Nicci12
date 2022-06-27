e2ccb0c974a5749d46b51a7822f581ef85606408
Nicola Dimant <96046479+Nicci12@users.noreply.github.com>
2022-06-27 16:12:13 +0300

    milestone 6 and 7 rough draft

--- java.js
+++ java.js
@@ -2,35 +2,56 @@ const input = document.getElementById("input");
 const btn = document.getElementById("btn");
 const number = document.getElementById("num");
 document.getElementById("error").style.color = "red";
-const usersListEle = document.getElementById("list");
 const resultsList = document.getElementById(`fullResults`);
-
-  
+const checkbox = document.getElementById("check");
+const result = document.getElementById("result");
+console.log(checkbox);
   ///////////////////////////best getList function
   
   async function getList() {
     const res = await fetch(`http://localhost:5050/getFibonacciResults`);
     const data = await res.json();
-    appendRows(data.results);
+    const sortedArray = data.results.sort((a,b) =>  b.createdDate - a.createdDate);
+    appendRows(sortedArray);
 
   }
   
   function appendRows(array) {
+    resultsList.innerHTML = '';
     const list = document.createElement(`div`);
-    for(let i=0; i<array.length;i++) {
+    for(let i=0; i<array.length;i++)
+    {
       const row = document.createElement(`div`);
       row.innerHTML = `The Fibonacci Of: <b>${array[i].number}</b> is <b>${array[i].result}</b>. Calculated at: ${new Date(array[i].createdDate)}`;
       list.append(row);
+      row.classList.add("fs-5", "border-bottom", "py-2", "border-secondary");
     }
     resultsList.append(list);
+  
   }
+
+
+// /////////////////////////////// checkbox check function
+function fibonacci(){
+  let x = document.getElementById("num").value;
+  let m = 0;
+  let y=1;
+  let z;
+  let i=0;
+
+  for(let i=2; i<=x; i++){
+    z=m+y;
+    m=y;
+    y=z;
+  }
+  result.innerHTML=`<strong><u>${y}</u></strong`;
+}
 
 
 /////////////////////////////////click events
 
 function handleClick() {
   document.getElementById("result").innerHTML = " ";
-  
   if (num.value > 50) {
     error.classList.add("errMessage");
     error.classList.remove("error");
@@ -43,14 +64,17 @@ function handleClick() {
     errMessage.innerText = "server error: 42 is the meaning of life";
     fibServer(document.getElementById("num").value);
     addLoader();
-  } else {
+  } else if(check.checked === false){
+      console.log("it worked")
+      getList();
+  }
+  else {
     error.classList.remove("error");
     error.classList.remove("errMessage");
     number.style.color = "black";
     addLoader();
     fibServer(document.getElementById("num").value);
-    removeLoader();
-    getList();
+  
   }
 }
 
@@ -69,6 +93,7 @@ function fibServer(num) {
   .then((data) => {
     document.getElementById("result").innerHTML = data["result"];
     addLoader();
+    getList();
   })
   
   .catch((err) => {
@@ -87,6 +112,11 @@ function removeLoader() {
 }
 
 ///////////////////main scope
+
+
+
+
+
 
 // getUsers(printUserToList);
 getList();
