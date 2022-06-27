1cdaaa920f6f351e048752b0d191c8b4441eab85
Nicola Dimant <96046479+Nicci12@users.noreply.github.com>
2022-06-27 13:07:05 +0300

    Milestone 6 updated

--- java.js
+++ java.js
@@ -1,41 +1,36 @@
-function removeLoader() {
-  $("#loader").removeClass("hide-loader");
-}
-
-function addLoader() {
-  $("#loader").addClass("hide-loader");
-}
-
-function fibServer(num) {
-  removeLoader();
-  fetch(`http://localhost:5050/fibonacci/${num}`)
-    .then((response) => {
-      if (!response.ok) {
-        response.text().then((errorT) => {
-          console.log(errorT);
-        });
-      }
-      return response.json();
-    })
-
-    .then((data) => {
-      document.getElementById("result").innerHTML = data["result"];
-      addLoader();
-    })
-
-    .catch((err) => {
-      document.getElementById("result").innerHTML = "";
-    });
-}
-
 const input = document.getElementById("input");
-const number = document.getElementById("num");
 const btn = document.getElementById("btn");
+const number = document.getElementById("num");
 document.getElementById("error").style.color = "red";
+const usersListEle = document.getElementById("list");
+const resultsList = document.getElementById(`fullResults`);
 
-btn.addEventListener("click", function () {
+  
+  ///////////////////////////best getList function
+  
+  async function getList() {
+    const res = await fetch(`http://localhost:5050/getFibonacciResults`);
+    const data = await res.json();
+    appendRows(data.results);
+
+  }
+  
+  function appendRows(array) {
+    const list = document.createElement(`div`);
+    for(let i=0; i<array.length;i++) {
+      const row = document.createElement(`div`);
+      row.innerHTML = `The Fibonacci Of: <b>${array[i].number}</b> is <b>${array[i].result}</b>. Calculated at: ${new Date(array[i].createdDate)}`;
+      list.append(row);
+    }
+    resultsList.append(list);
+  }
+
+
+/////////////////////////////////click events
+
+function handleClick() {
   document.getElementById("result").innerHTML = " ";
-
+  
   if (num.value > 50) {
     error.classList.add("errMessage");
     error.classList.remove("error");
@@ -52,46 +47,47 @@ btn.addEventListener("click", function () {
     error.classList.remove("error");
     error.classList.remove("errMessage");
     number.style.color = "black";
-    fibServer(document.getElementById("num").value);
     addLoader();
-    getUsers(printUserToList);
     fibServer(document.getElementById("num").value);
     removeLoader();
+    getList();
   }
-});
-
-const url= `http://localhost:5050/getFibonacciResults`;
-
-
-
-function getUsers(onSuccess) {
-    console.log('on success callback', onSuccess);
-
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            onSuccess(data);
-
-        })
-
-
 }
 
-
-    const usersListEle = document.getElementById('list');
-
-    function printUserToList(data) {
-    for (let i  of data.results 
-       .sort((a,b) => b["createdDate"]- a["createdDate"])
-       .slice(0,5))
-       
-       {
-       let date = new Date(i["createdDate"]);
-        document.createElement("li");
-        usersListEle.innerHTML +=  `<li>The Fibonacci of <b>${i["number"]}</b> is <b>${i["result"]}</b> Caculated at ${date} <li/>`;
-        usersListEle.appendChild("li");
+function fibServer(num) {
+  removeLoader();
+  fetch(`http://localhost:5050/fibonacci/${num}`)
+  .then((response) => {
+    if (!response.ok) {
+      response.text().then((errorT) => {
+        console.log(errorT);
+      });
     }
-    document.getElementById("fullResults").appendChild(usersListEle)
+    return response.json();
+  })
+  
+  .then((data) => {
+    document.getElementById("result").innerHTML = data["result"];
+    addLoader();
+  })
+  
+  .catch((err) => {
+    document.getElementById("result").innerHTML = "";
+  });
 }
 
+//////////////////spinner functions
+
+function addLoader() {
+  $("#loader").addClass("hide-loader");
+}
+
+function removeLoader() {
+  $("#loader").removeClass("hide-loader");
+}
+
+///////////////////main scope
+
+// getUsers(printUserToList);
+getList();
+btn.addEventListener("click", handleClick);
